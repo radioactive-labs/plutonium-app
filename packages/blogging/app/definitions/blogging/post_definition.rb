a45c64bd20039986e76c645d09d3b53c36a5e00c
TheDumbTechGuy <sfroelich01@gmail.com>
2024-11-08 15:09:50 +0000

    Post Detail View

--- packages/blogging/app/definitions/blogging/post_definition.rb
+++ packages/blogging/app/definitions/blogging/post_definition.rb
@@ -1,2 +1,6 @@
 class Blogging::PostDefinition < Blogging::ResourceDefinition
+  display :user, class: "col-span-full"
+  display :title, class: "col-span-full"
+  display :content, class: "col-span-full"
+  display :published_at, class: "col-span-full"
 end
