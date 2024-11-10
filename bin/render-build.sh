16b0ecff742ac745644aca678d6f86078da12f21
TheDumbTechGuy <sfroelich01@gmail.com>
2024-11-10 01:28:05 +0000

    add render deployment

---
+++ bin/render-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# https://docs.render.com/deploy-rails
+
+# Enable jemalloc for reduced memory usage and latency.
+if [ -z "${LD_PRELOAD+x}" ] && [ -f /usr/lib/*/libjemalloc.so.2 ]; then
+  export LD_PRELOAD="$(echo /usr/lib/*/libjemalloc.so.2)"
+fi
+
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+bundle exec rails db:prepare
